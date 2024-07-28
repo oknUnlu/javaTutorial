198f6ad7f8ac8a9ee3e7323ed162abd698aafdd5
okanu <okanunllu@gmail.com>
2024-07-28 16:25:25 +0300

    Day1

--- src/main/java/org/unluokan/Main.java
+++ src/main/java/org/unluokan/Main.java
@@ -3,5 +3,6 @@ package org.unluokan;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Simple test programing");
     }
 }
